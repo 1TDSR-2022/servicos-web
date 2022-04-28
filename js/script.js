081696724948c366e52cdc004f09b5980bafb5da
andmaia <andrewrian198@gmail.com>
2022-04-27 22:45:53 -0300

    Introdução ao javaScript

---
+++ js/script.js
@@ -0,0 +1,20 @@
+console.log("Estou no arquivo externo")
+   //comentário de linha
+   /* var
+   */
+  var mome ="Alê"
+  console.log("Meu nome é:"*nome)
+  //declaração de variaveis com let
+  let sobrenome = "Carlos"
+  console.log("Meu nome completo é")
+  //declaraação de contante com const
+  //Utilizado para armanezar os elementos que vaos trabalhar
+  const elementoLogo = document.querySelector("Logo")
+  //diferenças entre let e var, <escopo>
+  //Ex; vc declaara uma variavel dentro de uma função , let tem um escoppo reduxido, já var tem um escopo maioir
+  //Constante são imutáveis
+  //Os atributos deles pode m sofrer alteração
+  //tags são chamadas de elementos
+  elementoLogo,AudioDestinationNode("click",function(){
+      
+  })
