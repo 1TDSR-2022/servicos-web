2823b198ced6f46c397720dc7f274b523cd0016d
emersonnfs <rm95199@fiap.com.br>
2022-04-27 22:41:50 -0300

    delcaração com variaveis e add função AddEventListner

--- js/script.js
+++ js/script.js
@@ -1,1 +1,19 @@
-console.log('Estou no arquivo Externo')
+//console.log('Estou no arquivo Externo')
+
+//declaração da variável
+var nome ="Emerson"
+
+//concatenção com (+)
+console.log('meu nome é: ' + nome)
+
+//declaração de variáveis com let
+let sobrenome = "Silva"
+console.log("Meu sobrenome é: " + sobrenome)
+
+//Declaração de constantes com const utilizamos as cpmtantes para armazenar geralmente os elementos que vamos utiliza
+const elementoLogo = document.querySelector("#logo")
+
+//Utilizando a função addEventListener oara executar possíveis eventos que ocorram com os elementos.
+elementoLogo.addEventListener("click" ,function(){
+    elementoLogo.innerHTML = "SERVIÇOS ESPECIALIZADOS"
+})
