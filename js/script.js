2c2ab2eb7425c06906f93b6b8af0866dcc85fa99
RhayanBembem <rm95043@fiap.com.br>
2022-04-29 23:03:17 -0300

    Atribuindo array no js.

--- js/script.js
+++ js/script.js
@@ -20,3 +20,62 @@ const elementoLogo = document.querySelector("#logo")
 elementoLogo.addEventListener("click", function(){
     elementoLogo.innerHTML = "SERVIÇOS ESPECIALIZADOS"
 })
+
+
+//VETORES / ARRAYS/ LISTA/ COLEÇÕES
+var frutas = ["maçã","banana","laranja","uva","kiwi"]
+
+//verificando o primeiro elemento do array.
+//console.log("Primeiro elemento : " + frutas[0])
+
+//verificando o primeiro elemento do array.
+//console.log("Primeiro elemento : " + frutas[0])
+
+//Remover um item do inicio do array
+// frutas.shift()
+// console.table(frutas)
+
+//Adicionando um item ao inicio do array
+// frutas.unshift("acerola")
+// console.table(frutas)
+
+//Removendo um item através do indice do array do indexOF()
+//var posicao = frutas.indexOf('banana')
+//console.log("A ocorrência foi encontrada no indice :" + posicao)
+//frutas.splice(posicao,posicao)
+//console.table(frutas)
+
+//Interando um array com loop FOR
+//for(let x = 0; x < frutas.length; x++){
+//    console.log("A FRUTA É : "  + frutas[x])
+// }
+
+//Interando um array com loop FOR
+//for (let fruta of frutas) {
+//   console.log("A FRUTA É : " + frutas)
+// }
+
+//Interando um array com loop FOR
+//for (let indece in frutas) {
+//   console.log("A FRUTA É : " + frutas[indice])
+// }
+
+const elementoLogo = document.querySelector("#logo")
+
+//Utilizando a função AddEventLIstener para escutar possiveis
+//eventos que ocorram com 
+
+elementoLogo.addEventListener("click", function(){
+    elementoLogo.innerHTML = "SERVIÇOS ESPECIALIZADOS"
+})
+
+//Selecionando elementos com NOTAÇÂO css.
+//listDekli = ['li', 'li]
+const listDeLi = document.querySelectorAll("li")
+
+//for (const elementoLi of listDeLi) {
+//    console.log("Conteudo do <li> : " + elementoLi.innerHTML)
+//}
+
+
+
