890885834907078c4df03f4f09545e3fcbe468ef
Gustavo Meneguesso Costa <RM94838@fiap.com.br>
2022-04-29 23:00:51 -0300

    coisinhas de js ensinadas pelo grandíssimo Ale

--- JS/script.js
+++ JS/script.js
@@ -21,6 +21,65 @@ const elementoLogo = document.querySelector("#logo")
 
 //Utilizando a função AddEventListenner para esctar possíveis
 //eventos que ocorram com o elemento
-elementoLogo.addEventListener("click",dunction(){
+elementoLogo.addEventListener("click", function(){
     elementoLogo.innerHTML = "SERVIÇOS ESPECIALIZADOS"
 })
+
+//VETORES / ARRAYS / LISTAS / COLEÇÕES
+var frutas = ["maçã", "banana", "laranja", "uva", "kiwi"]
+console.table(frutas)
+//Imprimindo o conteúdo do array console.table(frutas)
+
+//Verificando o primeiro elemento do array
+// console.log("Primeiro elemento: " + frutas[0])
+
+//Verificando o tamanho do array
+//console.log("Tamanho do array: " + frutas.length)
+
+//Verificando o último elemento do array
+//console.log("Último elemento: " + frutas[frutas.lenght - 1])
+
+//Adicionando um item ao final do array
+//frutas.push("melancia")
+//console.table(frutas)
+
+//Remover um item do final do array
+//frutas.pop()
+//console.table(frutas)
+
+//Remover um item do início do array
+//frutas.shift()
+//console.table(frutas)
+
+//Adicionar um item do início do array
+//frutas.unshift("acerola")
+//console.table(frutas)
+
+//Removendo um item através do índice do array com indexOf()
+//var posicao = frutas.indexOf('banana')
+//console.log("A ocorrência foi encontrada no índice: " + posicao)
+//frutas.splice(posicao,posicao)
+//console.table(frutas)
+
+//Interando um array com loop FOR
+//for(let x = 0; x < 10; x++){
+//   console.log("A FRUTA É: " + frutas[x])
+//}
+
+//Interando um array com loop FOROF
+//for (let fruta of frutas) {
+//    console.log("A FRUTA É: " + fruta) 
+//}
+
+//Interando um array com loop FORIN
+//for (let indice of frutas) {
+//    console.log("A FRUTA É: " + fruta[indice]) 
+//}
+
+//Selecionando elementos com NOTAÇÃO CSS
+//listaDeLi = ['li', 'li']
+const listaDeLi = document.querySelectorAll("li")
+
+for (const elementoLi of listaDeLi){
+    console.log("Conteúdo do <li>: " + elementoLi.innerHTML)
+}
