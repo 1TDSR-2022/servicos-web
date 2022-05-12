3c041177ad12c36a70eb8ceaede96fea31af12da
ValentinaFuzaro <rm95741@fiap.com>
2022-05-12 13:08:02 -0300

    código comentado

--- js/script.js
+++ js/script.js
@@ -1,24 +1,24 @@
-console.log('Estou no arquivo externo!')
-
+/* console.log('Estou no arquivo externo!')
+ */
 //comentário de linha 
 /*
 comentário de bloco
 */
 
 //declaração de variáveis com var
-var nome = "Alê"
-//concatenação: utilizar o sinal de + 
-console.log("Meu nome é: " + nome)
-
+/* var nome = "Alê"
+ *///concatenação: utilizar o sinal de + 
+/* console.log("Meu nome é: " + nome)
+ */
 //declaração de variáveis com let
-let sobrenome = "Carlos"
+/* let sobrenome = "Carlos"
 console.log("Meu nome completo é: " + (nome + " " + sobrenome))
-
+ */
 //declaração de constantes com const
 //utilizamos as constantes para armazenar geralmente os elementos que vamos trabalhar
-const elementoLogo = document.querySelector("#logo")
-
+/* const elementoLogo = document.querySelector("#logo")
+ */
 //utilizando a função AddEventListenner para escutar possíveis eventos que ocorram com o elemento
-elementoLogo.addEventListener("click", function(){
+/* elementoLogo.addEventListener("click", function(){
     elementoLogo.innerHTML = "Serviços especializados"
-})
+}) */
